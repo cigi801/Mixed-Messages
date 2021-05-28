a448a512e8d647577a819d45242afa4dc6e758c9
cigi801 <cigiut@gmail.com>
2021-05-27 22:19:59 -0600

    changes to js

--- greetings.js
+++ greetings.js
@@ -1,6 +1,15 @@
 let userName = ' ';
-let greeting = '';
+
 const greetings = ['Bonjour', 'Hola', 'Ni Hao', 'Konnichiwa', 'Hello'];
 const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
 
-userName ? console.log(`${randomGreeting}, ${userName}`) : console.log(`${randomGreeting}`);
+
+
+userName = prompt('What is your name?');
+
+function greet(name)
+{
+    return randomGreeting + ' ' + userName + "!";
+}
+
+console.log(greet(userName));
