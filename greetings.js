14244bf9cd4e6861b527c29823cd14223c502867
cigi801 <cigiut@gmail.com>
2021-05-28 15:34:46 -0600

    changed code to show blank output at start

--- greetings.js
+++ greetings.js
@@ -4,10 +4,16 @@ const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
 
 
 function greet(){
-  let txtName = document.getElementById("txtName");
-  let txtOutput = document.getElementById("txtOutput");
-  let name = txtName.value;
-  txtOutput.value = randomGreeting + ", " + name + "!";
-}
-
-console.log(greet(name));
+    let txtName = document.getElementById("txtName");
+    let txtOutput = document.getElementById("txtOutput");
+    let name = txtName.value;
+    if(name == ""){
+    txtOutput.value = '';
+      return false;
+    } else {
+    txtOutput.value = randomGreeting + ", " + name + "!"; 
+    return true;
+    }
+  }
+  
+  console.log(greet(name));
