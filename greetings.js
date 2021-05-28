4c8fd5a063e087a84507d065bbd4af224dd7d55e
cigi801 <cigiut@gmail.com>
2021-05-28 14:25:36 -0600

    Adjustments to make output work properly on website

--- greetings.js
+++ greetings.js
@@ -1,15 +1,13 @@
-let userName = ' ';
-
 const greetings = ['Bonjour', 'Hola', 'Ni Hao', 'Konnichiwa', 'Hello'];
 const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
 
 
 
-userName = prompt('What is your name?');
-
-function greet(name)
-{
-    return randomGreeting + ' ' + userName + "!";
+function greet(){
+  let txtName = document.getElementById("txtName");
+  let txtOutput = document.getElementById("txtOutput");
+  let name = txtName.value;
+  txtOutput.value = randomGreeting + ", " + name + "!";
 }
 
-console.log(greet(userName));
+console.log(greet(name));
